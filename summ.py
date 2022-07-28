0b3893ccdb582e9be68995ceaeedd0693850b755
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-07-28 12:18:05 +0300

    Update summ.py

--- summ.py
+++ summ.py
@@ -14,3 +14,17 @@ def num_pow(n):
 x = main(seq, num_pow)
 
 print(x)
+
+#----------------------------------------------
+
+seq = [random.randint(1, 100) for i in range(100)]
+
+def main(seq, func):
+	tmp = 0
+	for i in seq:
+		tmp += func(i)
+	return tmp
+
+x = main(seq, lambda n: n ** 2)
+
+print(x)
